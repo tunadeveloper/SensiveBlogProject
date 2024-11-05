e6b4ee244a586ad6494b5f10198ffbc9b906606e
tunadeveloper <tunahancengizbusiness@gmail.com>
2024-11-05 15:46:58 +0300

    Update

---
+++ SensiveBlogProject.PresentationLayer/Controllers/CategoryController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SensiveBlogProject.BusinessLayer.Abstract;
+using SensiveBlogProject.DataAccessLayer.Context;
+using SensiveBlogProject.EntityLayer.Concrete;
+
+namespace SensiveBlogProject.PresentationLayer.Controllers
+{
+	public class CategoryController : Controller
+	{
+		private readonly ICategoryService _categoryService;
+
+		public CategoryController(ICategoryService categoryService)
+		{
+			_categoryService = categoryService;
+		}
+
+		public IActionResult CategoryList()
+		{
+			var values = _categoryService.TGetAll();
+			return View(values);
+		}
+
+		public IActionResult CreateCategory()
+		{
+			return View();
+		}
+
+		[HttpPost]	
+		public IActionResult CreateCategory(Category category)
+		{
+			_categoryService.TInsert(category);
+			return RedirectToAction("CategoryList");
+		}
+
+		public IActionResult DeleteCategory(int id)
+		{
+			_categoryService.TDelete(id);
+			return RedirectToAction("CategoryList");
+		}
+
+		public IActionResult UpdateCategory(int id)
+		{
+			var value = _categoryService.TGetById(id);
+			return View(value);
+		}
+
+		[HttpPost]
+        public IActionResult UpdateCategory(Category category)
+		{
+			_categoryService.TUpdate(category);
+			return RedirectToAction("CategoryList");
+		}
+
+    }
+}
