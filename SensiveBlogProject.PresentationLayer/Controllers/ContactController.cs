48737398254a9963c6cde4a7f055deab621f8f5f
tunadeveloper <tunabusiness25@gmail.com>
2024-12-04 21:45:56 +0300

    Updated

---
+++ SensiveBlogProject.PresentationLayer/Controllers/ContactController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SensiveBlogProject.BusinessLayer.Abstract;
+using SensiveBlogProject.BusinessLayer.Concrete;
+using SensiveBlogProject.EntityLayer.Concrete;
+
+namespace SensiveBlogProject.PresentationLayer.Controllers
+{
+
+    public class ContactController : Controller
+    {
+        private readonly IContactService _contactService;
+
+        public ContactController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            contact.Created = DateTime.Now;
+            contact.Status = false;
+            try
+            {
+                _contactService.TInsert(contact);
+                return Json(new { success = true, message = "Mesajınız başarıyla gönderildi!" });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "Bir hata oluştu. Lütfen tekrar deneyin." });
+            }
+        }
+
+    }
+}
