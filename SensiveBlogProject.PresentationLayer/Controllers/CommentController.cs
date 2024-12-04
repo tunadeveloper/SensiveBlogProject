4b52f2b31099bc5811945b6c3a1cb7dab670b2a7
tunadeveloper <tunabusiness25@gmail.com>
2024-12-04 19:28:20 +0300

    Updated

--- SensiveBlogProject.PresentationLayer/Controllers/CommentController.cs
+++ SensiveBlogProject.PresentationLayer/Controllers/CommentController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SensiveBlogProject.BusinessLayer.Abstract;
 using SensiveBlogProject.EntityLayer.Concrete;
-
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
 namespace SensiveBlogProject.PresentationLayer.Controllers
 {
     public class CommentController : Controller
@@ -23,7 +25,6 @@ namespace SensiveBlogProject.PresentationLayer.Controllers
         public PartialViewResult AddComment(int id)
         {
             ViewBag.id = id;
-
             return PartialView();
         }
 
@@ -31,13 +32,39 @@ namespace SensiveBlogProject.PresentationLayer.Controllers
         [HttpPost]
         public IActionResult AddComment(Comment comment)
         {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return Json(new
+                {
+                    success = false,
+                    message = "Yorum eklemek için giriş yapmalısınız.",
+                    redirect = "/Login/Index"
+                });
+            }
+
+            string userIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (!int.TryParse(userIdString, out int userId))
+            {
+                return Json(new { success = false, message = "Kullanıcı ID'si doğru formatta değil." });
+            }
+
+            comment.AppUserId = userId;
+            comment.Status = true;
             comment.CreatedDate = DateTime.Now;
-            //comment.ArticleId = 0;
-            comment.AppUserId = 3;
 
-            _commentService.TInsert(comment);
+            try
+            {
+                _commentService.TInsert(comment);
+                return Json(new { success = true, message = "Yorumunuz başarıyla eklendi!" });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = $"Yorum eklenirken bir hata oluştu: {ex.Message}" });
+            }
 
-            return Json(new { success = true, message = "Yorumunuz başarıyla eklendi!" });
+           
         }
+
+
     }
 }
